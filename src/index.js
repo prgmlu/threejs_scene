07995a1573e8a474f70f9d88825463836df53a9f
natali3 <natali@obsessvr.com>
2021-10-27 18:35:41 -0400

    fix

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import Scene from "./Scene";
 export {default as Hotspot} from './_publicUI/Hotspot';
 export {default as HotspotMarker} from './three-base-components/Markers/HotspotMarker';
-export {default as ImageMarkerClass} from './three-base-components/Markers/ImageMarker';
+export {default as ImageMarker} from './three-base-components/Markers/ImageMarker';
 
 // import ImageMarker from './three-base-components/Markers/ImageMarker';
 // export ImageMarker;
-export var ImageMarker = ImageMarkerClass;
-export var imageMarker = ImageMarkerClass;
+// export var a = ImageMarker;
+
 export default Scene;
