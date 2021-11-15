88a99a82e377d3b49e92a5506285d6dcd47ab5fc
natali3 <natali@obsessvr.com>
2021-11-15 18:19:05 -0500

    extract DebugUI into separate component

--- src/Scene/index.jsx
+++ src/Scene/index.jsx
@@ -4,6 +4,7 @@ import ThreeController from '../three-controls/ThreeController';
 import {initThreeJSScene, setupRenderer, setupCamera } from './setupThreeEditor';
 import { threeEditorMouseEvents } from './threeEditorMouseEvents';
 import { Background, ColliderSphere } from '../three-background';
+import DebugUI from "../utils/DebugUI";
 import './main.scss';
 
 
@@ -214,46 +215,6 @@ const Scene = (props) => {
 };
 
 
-/**
- * DebugUI - could be used to see memory usage and other interesting things :)
- */
-const DebugUI=({renderer, glContext, scene})=>{
-    //Never use it on prod
-    if(!['localhost','127.0.0.1'].includes(window.location.hostname )) return false;
 
-    // Note: WebGLRenderer.forceContextLoss() and WebGLRenderer.forceContextRestore()
-    // are used to simulate a context loss and restore based on the WebGL extension WEBGL_lose_context.
-    // If you have a real context loss, the mentioned events should be triggered automatically.
-    const ext = glContext.getExtension('WEBGL_lose_context');
-
-
-    const restore=(e)=>{
-        ext.restoreContext();
-        renderer.forceContextRestore()
-    }
-
-    const {info} = renderer;
-    const cap = Object.entries(renderer.capabilities).reduce((acc, [k,v])=>{
-        return ['isWebGL2', 'maxCubemapSize','maxTextures','maxTextureSize'].includes(k) ?  {...acc, [k]: v} : acc;
-    },{});
-
-    const Tr=({label, data, style={}})=>(<tr style={style}>
-        <td>{label}:</td>
-        <td>{ data }</td>
-    </tr>);
-
-    return(<div style={{display:'block', width:'100%', fontSize:'12px', border:'1px dashed', padding:'1em', wordBreak: 'break-word'}}>
-        <table >
-            <tbody>
-                <Tr label='Scene' data={JSON.stringify({children: scene.children.length}) } style={{minWidth:'10em'}}/>
-                <Tr label='Memory' data={JSON.stringify(info.memory)}/>
-                <Tr label='Render' data={JSON.stringify(info.render)}/>
-                <Tr label='Capabilities' data={JSON.stringify(cap)}/>
-                <Tr label='force Context Loss' data={(<button style={{backgroundColor: '#CB203FBC', color:'#fff', border: 'none' }}  onClick={e=>renderer.forceContextLoss()}>Call</button>)}/>
-                <Tr label='force Context Restore' data={(<button style={{backgroundColor: '#85f141bd', color:'#fff', border: 'none' }} onClick={restore}>Call</button>)}/>
-            </tbody>
-        </table>
-    </div>)
-}
 
 export default Scene;
---
+++ src/utils/DebugUI.js
@@ -0,0 +1,48 @@
+import React from 'react';
+
+
+
+
+/**
+ * DebugUI - could be used to see memory usage and other interesting things :)
+ */
+export default function DebugUI({renderer, glContext, scene}){
+    //Never use it on prod
+    if(!['localhost','127.0.0.1','0.0.0.0', '192.168.1.187'].includes(window.location.hostname )) return false;
+
+    // Note: WebGLRenderer.forceContextLoss() and WebGLRenderer.forceContextRestore()
+    // are used to simulate a context loss and restore based on the WebGL extension WEBGL_lose_context.
+    // If you have a real context loss, the mentioned events should be triggered automatically.
+    const ext = glContext.getExtension('WEBGL_lose_context');
+    const supportedExt = glContext.getSupportedExtensions() ;
+
+    console.log('>BebugUI', 'color:blue', {supportedExt});
+
+    const restore=(e)=>{
+        ext.restoreContext();
+        renderer.forceContextRestore()
+    }
+
+    const {info} = renderer;
+    const cap = Object.entries(renderer.capabilities).reduce((acc, [k,v])=>{
+        return ['isWebGL2', 'maxCubemapSize','maxTextures','maxTextureSize'].includes(k) ?  {...acc, [k]: v} : acc;
+    },{});
+
+    const Tr=({label, data, style={}})=>(<tr style={style}>
+        <td>{label}:</td>
+        <td>{ data }</td>
+    </tr>);
+
+    return(<div style={{display:'block', width:'100%', fontSize:'12px', border:'1px dashed', padding:'1em', wordBreak: 'break-word'}}>
+        <table >
+            <tbody>
+            <Tr label='Scene' data={JSON.stringify({children: scene.children.length}) } style={{minWidth:'10em'}}/>
+            <Tr label='Memory' data={JSON.stringify(info.memory)}/>
+            <Tr label='Render' data={JSON.stringify(info.render)}/>
+            <Tr label='Capabilities' data={JSON.stringify(cap)}/>
+            <Tr label='force Context Loss' data={(<button style={{backgroundColor: '#CB203FBC', color:'#fff', border: 'none' }}  onClick={e=>renderer.forceContextLoss()}>Call</button>)}/>
+            <Tr label='force Context Restore' data={(<button style={{backgroundColor: '#85f141bd', color:'#fff', border: 'none' }} onClick={restore}>Call</button>)}/>
+            </tbody>
+        </table>
+    </div>)
+}
