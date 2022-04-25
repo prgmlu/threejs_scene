a6ca2e3c8c52bb936bbbe02882a05f63e36db843
mostafa elkabany <mostafa@ardentpeak.com>
2022-04-25 23:27:42 +0300

    green screen

--- src/in-scene-video/InSceneVidComponent.jsx
+++ src/in-scene-video/InSceneVidComponent.jsx
@@ -1,21 +1,8 @@
 import * as THREE from 'three';
 import React, { useEffect, useRef } from 'react';
+import { vShader, fShader } from './shaders';
 
-const createVidDom = function (src) {
-	var video = document.createElement('video');
-	video.src = src;
-	video.setAttribute('webkit-playsinline', '');
-	video.crossOrigin = 'anonymous';
-	video.setAttribute('playsinline', '');
-	video.setAttribute('loop', 'loop');
-	video.setAttribute('autoplay', 'autoplay');
-	video.muted = true;
-	video.play();
-	return video;
-};
-
-export const createVidScreen = function (vid_source, transform) {
-	const video = createVidDom(vid_source);
+export const createNormalVidMaterial = function (video, geometry) {
 	const videoTexture = new THREE.VideoTexture(video);
 	videoTexture.minFilter = THREE.LinearFilter;
 	videoTexture.magFilter = THREE.LinearFilter;
@@ -33,34 +20,129 @@ export const createVidScreen = function (vid_source, transform) {
 
 	var videoMaterial = new THREE.MeshBasicMaterial({ ...materialParams });
 
-	const screen = new THREE.PlaneGeometry(1, 1);
-	const videoScreen = new THREE.Mesh(screen, videoMaterial);
-
-	transform.decompose(
-		videoScreen.position,
-		videoScreen.quaternion,
-		videoScreen.scale,
-	);
-	// videoScreen.position.set(position.x, position.y, position.z);
-	// videoScreen.rotation.set(rotation.x, rotation.y, rotation.z);
-	// videoScreen.scale.set(scale.x, scale.y, scale.z);
-
-	return videoScreen;
+	return videoMaterial;
 };
 
+export const createGreenScreenMaterial = function (keyColor){
+	var width = 1280;
+	var height = 720;
+	var keyColor = keyColor;
+
+	var vals = {
+        uniforms: {
+          tex: {
+            value: null
+          },
+          keyColor: {
+            value: new THREE.Color(keyColor)
+          },
+          texWidth: {
+            value: width
+          },
+          texHeight: {
+            value: height
+          },
+          similarity: {
+            value: .4
+          },
+          smoothness: {
+            value: 0.08
+          },
+          spill: {
+            value: .1
+          }
+
+        },
+        vertexShader: vShader,
+        fragmentShader: fShader,
+
+        transparent: true,
+        opacity: 0
+      };
+	var sMat = new THREE.ShaderMaterial();
+	sMat.setValues(vals);
+
+  return sMat;
+}
+
+
+const createVidDomForNormalVid = function (src) {
+	var video = document.createElement('video');
+	video.src = src;
+	video.setAttribute('webkit-playsinline', '');
+	video.crossOrigin = 'anonymous';
+	video.setAttribute('playsinline', '');
+	video.setAttribute('loop', 'loop');
+	video.setAttribute('autoplay', 'autoplay');
+	video.muted = true;
+	video.play();
+	return video;
+};
+
+
+
+const createVidDomForGreenScreen = function (src, scene, vidMesh) {
+	var video = document.createElement('video');
+	video.addEventListener('loadeddata', () => {
+
+		window.addEventListener('click', () => {
+			video.play();
+			scene.add(vidMesh);
+		})
+		window.addEventListener('touchend', () => {
+			video.play();
+			scene.add(vidMesh);
+		})
+	})
+	video.src = src;
+	video.setAttribute('webkit-playsinline', '');
+	video.crossOrigin = 'anonymous';
+	video.setAttribute('playsinline', '');
+	//   video.setAttribute('loop', 'loop');
+	// video.setAttribute('autoplay', 'false');
+	video.muted = false;
+	return video;
+}
+
+
 const InSceneVidComponent = (props) => {
-	// let {src, position, scale, rotation, scene, sceneRef} = props;
-	let { src, transform, scene, sceneRef } = props;
+	let { src, scene, transform, keyColor, sceneRef } = props;
 	scene = sceneRef?.current || scene;
-	var vidScreen = useRef(null);
+	var vidMesh = useRef(null);
+
+
 	useEffect(() => {
-		// vidScreen.current = createVidScreen(src, position, scale, rotation);
+		var geometry = new THREE.PlaneGeometry(1, 1);
+		geometry.scale(- 1, 1, 1);
+
+		if (keyColor) {
+			var material = createGreenScreenMaterial(keyColor);
+			vidMesh.current = new THREE.Mesh(geometry, material);
+			const video = createVidDomForGreenScreen(src, scene, vidMesh.current);
+
+			var texture = new THREE.VideoTexture(video);
+			vidMesh.current.material.uniforms.tex.value = texture;
+		}
+
+		else {
+			const video = createVidDomForNormalVid(src);
+			var material = createNormalVidMaterial(video, geometry);
+			vidMesh.current = new THREE.Mesh(geometry, material);
+			 
+			scene.add(vidMesh.current);
+		}
+
 		let transformMatrix = new THREE.Matrix4();
 		transformMatrix.elements = transform;
-		vidScreen.current = createVidScreen(src, transformMatrix);
-		scene.add(vidScreen.current);
+
+		transformMatrix.decompose(
+			vidMesh.current.position,
+			vidMesh.current.quaternion,
+			vidMesh.current.scale,
+		);
+
 		return () => {
-			scene.remove(vidScreen.current);
+			scene.remove(vidMesh.current);
 		};
 	}, [scene]);
 
