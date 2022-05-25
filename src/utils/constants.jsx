3175930b249ab8df1b0a2aa2086f38888a4de910
Yashodhan Kumthekar <yashodhan@obsessvr.com>
2022-05-25 12:39:01 -0500

    Updates default hotspot image

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -3,6 +3,6 @@ export const MAX_POLAR_ANGLE = 120;
 export const MIN_AZIMUTH_ANGLE = 0;
 export const MAX_AZIMUTH_ANGLE = 359;
 export const DEFAULT_HOTSPOT_IMAGE =
-	'https://cdn.obsess-vr.com/default_icons/black_dot_in_white_circle.svg';
+	'https://cdn.obsess-vr.com/default_icons/dot_with_ring_white.svg';
 export const DEFAULT_PLAY_ICON = 'https://cdn.obsess-vr.com/play.svg';
 export const DEFAULT_PAUSE_ICON = 'https://cdn.obsess-vr.com/pause.svg';
