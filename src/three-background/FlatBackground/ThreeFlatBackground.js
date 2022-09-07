5dd57429dd40c2e15f6f70f73709a1f0ea1c0d1e
Yashodhan Kumthekar <yashodhan@obsessvr.com>
2022-09-07 16:04:49 -0400

    Updates

--- src/three-background/FlatBackground/ThreeFlatBackground.js
+++ src/three-background/FlatBackground/ThreeFlatBackground.js
@@ -18,7 +18,7 @@ export default class ThreeFlatBackground extends ThreeSceneObject {
 		const material = new THREE.MeshBasicMaterial({
 			depthTest: false,
 			depthWrite: false,
-			color: 'black',
+			// color: 'black',
 			// wireframe:true
 		});
 
