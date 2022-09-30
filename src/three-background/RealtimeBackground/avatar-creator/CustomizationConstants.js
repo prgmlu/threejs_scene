f581aa774e185bb7f7e34250c1744792fd95bc2f
mostafa elkabany <mostafa@ardentpeak.com>
2022-09-30 19:30:38 +0300

    ui

--- src/three-background/RealtimeBackground/avatar-creator/CustomizationConstants.js
+++ src/three-background/RealtimeBackground/avatar-creator/CustomizationConstants.js
@@ -16,8 +16,8 @@ export const SHADOW_MAP_TYPE = THREE.PCFShadowMap;
 export const FULL_BODY_VIEW_POSITION = [0,0,0]
 export const FULL_BODY_VIEW_ROTATION = [0,0,0]
 
-export const ZOOM_ON_FACE_POSITION = [0, -1.529, -0.36]
+export const ZOOM_ON_FACE_POSITION = [0, -1.529, -0.46]
 export const ZOOM_ON_FACE_ROTATION = [0,0,0]
 
 export const AVATAR_EDITOR_AVATAR_POSITION = [0, -0.835, -2.25]
-export const AVATAR_EDITOR_AVATAR_ROTATION = [0.5, 0, 0]
+export const AVATAR_EDITOR_AVATAR_ROTATION = [0.35, 0, 0]
